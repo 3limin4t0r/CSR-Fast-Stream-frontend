8bbffa9cedbcaec1aae65b821fed5acb5f9a5382
jaffamonkey <paul@jaffamonkey.com>
2014-12-10 14:26:03 +0000

    added behat code and updated behat.yml to use more test clients and composer.json with required extensions

--- test/features/bootstrap/TransformCore/Bundle/AppBundle/Features/Context/FeatureContext.php
+++ test/features/bootstrap/TransformCore/Bundle/AppBundle/Features/Context/FeatureContext.php
@@ -4,24 +4,356 @@ namespace TransformCore\Bundle\AppBundle\Features\Context;
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Behat\Tester\Exception\PendingException;
+use Behat\MinkExtension\Context\MinkContext;
+use Guzzle\Http;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
-use Behat\MinkExtension\Context\MinkContext;
 
 /**
- * Defines application features from the specific context.
+ * Features context.
  */
 class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+
 {
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * The response given from the resource.
      */
+    protected $response;
+    protected $json_response;
+    private $_response;
+    public $_client;
+    private $_parameters = array();
+
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     * @param   array $parameters Context parameters (set them up through behat.yml)
+     */
+
     public function __construct()
     {
+        $this->baseUrl = 'http://jaffamonkey.com';
+        $client = new Http\Client(['base_url' => 'http://jaffamonkey.com']);
+        $this->_client = $client;
     }
+
+    public function getParameter($name)
+    {
+        if (count($this->_parameters) === 0) {
+            throw new \Exception('Parameters not loaded!');
+        } else {
+            $parameters = $this->_parameters;
+            return (isset($parameters[$name])) ? $parameters[$name] : null;
+        }
+    }
+
+    public function spin($lambda, $wait = 60)
+    {
+        for ($i = 0; $i < $wait; $i++) {
+            try {
+                if ($lambda($this)) {
+                    return true;
+                }
+            } catch (\Exception $e) {
+                // do nothing
+            }
+            sleep(1);
+        }
+
+        $backtrace = debug_backtrace();
+
+        throw new \Exception(
+            "Timeout thrown by " . $backtrace[1]['class'] . "::" . $backtrace[1]['function'] . "()\n" .
+            $backtrace[1]['file'] . ", line " . $backtrace[1]['line']
+        );
+    }
+
+    /**
+     * Adds Basic Authentication header to next request.
+     *
+     * @param string $username
+     * @param string $password
+     *
+     * @Given /^I am authenticating as "([^"]*)" with "([^"]*)" password$/
+     */
+    public function iAmAuthenticatingAs($username, $password)
+    {
+        // $this->removeHeader('Authorization');
+        //$this->authorization = base64_encode($username . ':' . $password);
+        $this->addHeader('X-USER-ID:' . $username);
+    }
+
+    /**
+     * Adds header
+     *
+     * @param string $header
+     */
+    protected function addHeader($header)
+    {
+        $this->headers[] = $header;
+    }
+
+    private $headers = array();
+
+    /**
+     * @When /^I request "([^"]*)"$/
+     */
+    public function iRequest($uri)
+    {
+        $request = $this->_client->get($uri);
+        $this->_response = $request->send();
+        $request->send();
+    }
+
+    /**
+     * @Then /^the response should be JSON$/
+     */
+    public function theResponseShouldBeJson()
+    {
+
+        $data = json_decode($this->_response->getBody());
+        if (empty($data)) {
+            throw new Exception("Response was not JSON\n" . $this->_response);
+        }
+    }
+
+    /**
+     * @Given /^the response has a "([^"]*)" property$/
+     */
+    public function theResponseHasAProperty($propertyName)
+    {
+        $data = json_decode($this->_response->getBody(true));
+        if (!empty($data)) {
+            if (!isset($data->$propertyName)) {
+                throw new Exception("Property '" . $propertyName . "' is not set!\n");
+            }
+        } else {
+            throw new Exception("Response was not JSON\n" . $this->_response->getBody(true));
+        }
+    }
+
+    /**
+     * @Then /^the "([^"]*)" property equals "([^"]*)"$/
+     */
+    public function thePropertyEquals($propertyName, $propertyValue)
+    {
+        $data = json_decode($this->_response->getBody(true));
+        if (!empty($data)) {
+            if (!isset($data->$propertyName)) {
+                throw new Exception("Property '" . $propertyName . "' is not set!\n");
+            }
+            if ($data->$propertyName !== $propertyValue) {
+                throw new \Exception('Property value mismatch! (given: ' . $propertyValue . ', match: ' . $data->$propertyName . ')');
+            }
+        } else {
+            throw new Exception("Response was not JSON\n" . $this->_response->getBody(true));
+        }
+        echo $data->$propertyName;
+    }
+
+    /**
+     * @Given /^I fill form with:$/
+     */
+    public function fillForm(TableNode $table)
+    {
+        $page = $this->getSession()->getPage();
+
+        foreach ($table->getRows() as $row) {
+            list($fieldSelector, $value) = $row;
+
+            $field = $page->findField($fieldSelector);
+            if (empty($field)) {
+                $field = $this->getSession()->getDriver()->find('//label[contains(normalize-space(string(.)), "' . $fieldSelector . '")]');
+                if (!empty($field)) {
+                    $field = current($field);
+                }
+            }
+
+            $tag = strtolower($field->getTagName());
+
+            if ($tag == 'textarea') {
+                $page->fillField($fieldSelector, $value);
+            } elseif ($tag == 'select') {
+                if ($field->hasAttribute('multiple')) {
+                    foreach (explode(',', $value) as $index => $option) {
+                        $page->selectFieldOption($fieldSelector, trim($option), true);
+                    }
+                } else {
+                    $page->selectFieldOption($fieldSelector, $value);
+                }
+            } elseif ($tag == 'input') {
+                $type = strtolower($field->getAttribute('type'));
+                if ($type == 'checkbox' || $type == 'radio') {
+                    if (strtolower($value) == 'yes') {
+                        $page->checkField($fieldSelector);
+                    } else {
+                        $page->uncheckField($fieldSelector);
+                    }
+//                } elseif ($type == 'radio') {
+//                    // TODO: handle radio
+                } else {
+                    $page->fillField($fieldSelector, $value);
+                }
+            } elseif ($tag == 'label') {
+                foreach (explode(',', $value) as $option) {
+                    $option = $this->fixStepArgument(trim($option));
+                    $field->getParent()->checkField($option);
+                }
+            }
+        }
+    }
+
+    /**
+     * @Given /^I should see form with:$/
+     */
+    public function assertFormContain(TableNode $table)
+    {
+        foreach ($table->getRows() as $row) {
+            list($field, $value) = $row;
+
+            $node = $this->getSession()->getPage()->findField($field);
+            if (empty($node)) {
+                $node = $this->getSession()->getDriver()->find('//label[contains(normalize-space(string(.)), "' . $field . '")]');
+                if (!empty($node)) {
+                    $node = current($node);
+                }
+            }
+
+            if (null === $node) {
+                throw new \Behat\Mink\Exception\ElementNotFoundException($this->getSession(), 'form field', 'id|name|label|value', $field);
+            }
+
+            if ($node->getTagName() == 'input' && in_array($node->getAttribute('type'), array('checkbox', 'radio'))) {
+                $actual = $node->isChecked() ? 'YES' : 'NO';
+            } elseif ($node->getTagName() == 'select') {
+                $actual = $node->getValue();
+                if (!is_array($actual)) {
+                    $actual = array($actual);
+                }
+
+                $options = array();
+                $optionNodes = $this->getSession()->getDriver()->find($node->getXpath() . "/option");
+                foreach ($optionNodes as $optionNode) {
+                    $options[$optionNode->getValue()] = $optionNode->getText();
+                    $options[$optionNode->getText()] = $optionNode->getText();
+                }
+                foreach ($actual as $index => $optionValue) {
+                    if (isset($options[$optionValue])) {
+                        $actual[$index] = $options[$optionValue];
+                    }
+                }
+            } elseif ($node->getTagName() == 'label') {
+                foreach (explode(',', $value) as $option) {
+                    $option = $this->fixStepArgument(trim($option));
+                    $this->assertSession()->checkboxChecked($option);
+                }
+                return true;
+            } else {
+                $actual = $node->getValue();
+            }
+
+            if (is_array($actual)) {
+                $actual = join(',', $actual);
+            }
+            $regex = '/^' . preg_quote($value, '$/') . '/ui';
+
+            if (!preg_match($regex, $actual)) {
+                $message = sprintf('The field "%s" value is "%s", but "%s" expected.', $field, $actual, $value);
+                throw new \Behat\Mink\Exception\ExpectationException($message, $this->getSession());
+            }
+        }
+    }
+
+    /**
+     * @Given /^I fill in "(?P<field>(?:[^"]|\\")*)" with:$/
+     */
+    public function iFillInWith($field, PyStringNode $string)
+    {
+        $field = $this->fixStepArgument($field);
+        $value = $this->fixStepArgument($string->getRaw());
+        $this->getSession()->getPage()->fillField($field, $value);
+    }
+
+    /**
+     * @Given /^the "(?P<field>[^"]*)" field should contain:$/
+     */
+    public function assertFieldShouldContain($field, PyStringNode $string)
+    {
+        $this->assertSession()->fieldValueEquals($field, $string->getRaw());
+    }
+
+    /**
+     * Checks, that form field with specified id|name|label|value has specified value.
+     *
+     * @Then /^the "(?P<field>(?:[^"]|\\")*)" multiple field should contain "(?P<value>(?:[^"]|\\")*)"$/
+     */
+    public function assertFieldContains($field, $value)
+    {
+        $node = $this->assertSession()->fieldExists($field);
+        $actual = $node->getValue();
+        if (is_array($actual)) {
+            $actual = join(',', $actual);
+        }
+        $regex = '/^' . preg_quote($value, '$/') . '/ui';
+
+        if (!preg_match($regex, $actual)) {
+            $message = sprintf('The field "%s" value is "%s", but "%s" expected.', $field, $actual, $value);
+            throw new \Behat\Mink\Exception\ExpectationException($message, $this->getSession());
+        }
+    }
+
+    /**
+     * Returns fixed step argument (with \\" replaced back to ").
+     *
+     * @param string $argument
+     *
+     * @return string
+     */
+    protected function fixStepArgument($argument)
+    {
+        return str_replace('\\"', '"', $argument);
+    }
+
+
+    /**
+     * @When /^I check the "([^"]*)" radio button$/
+     */
+    public function iCheckTheRadioButton($radioLabel)
+    {
+        $radioButton = $this->getSession()->getPage()->findField($radioLabel);
+        if (null === $radioButton) {
+            throw new \Exception("Cannot find radio button " . $radioLabel);
+        }
+        $value = $radioButton->getAttribute('value');
+        $this->getSession()->getDriver()->click($radioButton->getXPath());
+        sleep(1);
+    }
+
+    /**
+     * @Then /^count of "([^"]*)" instances of "(?P<text>[^"]*)" exists on page$/
+     */
+    public function thePersonsAreUnlinked($count, $area)
+    {
+        $str = $this->getSession()->getPage()->getContent();
+        $count2 = substr_count($str, $area);
+        if ($count === $count2) {
+            echo 'The count of:' . $count2 . ' is correct';
+        } else {
+            throw new \Exception("Count " . $count . " expected but got " . $count2);
+        }
+    }
+
+    /**
+     * @Then /^the server code response should be (\d+)$/
+     */
+    public function theResponseStatusCodeShouldBe($httpStatus)
+    {
+        if ((string)$this->_response->getStatusCode() !== $httpStatus) {
+            throw new \Exception('HTTP code does not match ' . $httpStatus .
+                ' (actual: ' . $this->_response->getStatusCode() . ')');
+        }
+    }
+
 }
+
+?>
