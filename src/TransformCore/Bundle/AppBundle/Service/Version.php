6d8e694a2e35ca8f031c12d811e1f439f181ad2d
Eddie Jaoude <eddie@jaoudestudios.com>
2014-12-18 09:13:36 +0000

    Version service & Spec Test

---
+++ src/TransformCore/Bundle/AppBundle/Service/Version.php
@@ -0,0 +1,27 @@
+<?php
+namespace TransformCore\Bundle\AppBundle\Service;
+
+class Version
+{
+
+    /**
+     * @var string
+     */
+    private $version;
+
+    /**
+     * @param string $version
+     */
+    public function __construct($version)
+    {
+        $this->version = $version;
+    }
+
+    /**
+     * @return string
+     */
+    public function current()
+    {
+        return file_get_contents($this->version);
+    }
+}
