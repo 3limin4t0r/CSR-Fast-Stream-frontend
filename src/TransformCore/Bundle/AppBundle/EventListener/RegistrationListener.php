17fc3bd1be8713e4ce2768f78ba35bb8f10b1148
Eddie Jaoude <eddie@jaoudestudios.com>
2015-01-09 06:31:58 +0000

    [CSR-6] Successful Register redirect to Account

--- src/TransformCore/Bundle/AppBundle/EventListener/RegistrationListener.php
+++ src/TransformCore/Bundle/AppBundle/EventListener/RegistrationListener.php
@@ -2,9 +2,12 @@
 
 namespace TransformCore\Bundle\AppBundle\EventListener;
 
+use FOS\UserBundle\Event\FormEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use FOS\UserBundle\Event\UserEvent;
 use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Class UserRegistrationListener
@@ -17,12 +20,23 @@ class RegistrationListener implements EventSubscriberInterface
 {
 
     /**
+     * @var UrlGeneratorInterface
+     */
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
      * @return array
      */
     public static function getSubscribedEvents()
     {
         return array(
             FOSUserEvents::REGISTRATION_INITIALIZE => 'onRegistrationInit',
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess',
         );
     }
 
@@ -35,5 +49,15 @@ class RegistrationListener implements EventSubscriberInterface
     {
         $user = $event->getUser();
         $user->setUsername(uniqid(time(), true));
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function onRegistrationSuccess(FormEvent $event)
+    {
+        $url = $this->router->generate('transform_core_app_account');
+
+        $event->setResponse(new RedirectResponse($url));
     }
 }
--- src/TransformCore/Bundle/AppBundle/Tests/Spec/TransformCore/Bundle/AppBundle/EventListener/RegistrationListenerSpec.php
+++ src/TransformCore/Bundle/AppBundle/Tests/Spec/TransformCore/Bundle/AppBundle/EventListener/RegistrationListenerSpec.php
@@ -2,19 +2,27 @@
 
 namespace Spec\TransformCore\Bundle\AppBundle\EventListener;
 
+use FOS\UserBundle\Event\FormEvent;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use FOS\UserBundle\Event\UserEvent;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use TransformCore\Bundle\CsrFastStreamBundle\Entity\User;
 
 class RegistrationListenerSpec extends ObjectBehavior
 {
+
+    function let(UrlGeneratorInterface $router)
+    {
+        $this->beConstructedWith($router, true);
+    }
+    
     function it_is_initializable()
     {
         $this->shouldHaveType('TransformCore\Bundle\AppBundle\EventListener\RegistrationListener');
     }
 
-    function its_on_registration_init(UserEvent $userEvent, User $user)
+    function its_on_registration_init(UrlGeneratorInterface $router, UserEvent $userEvent, User $user)
     {
         $user
             ->setUsername(Argument::type('string'))
@@ -26,5 +34,21 @@ class RegistrationListenerSpec extends ObjectBehavior
             ->willReturn($user);
 
         $this->onRegistrationInit($userEvent);
+    }
+
+    function its_on_registration_success(UrlGeneratorInterface $router, FormEvent $event)
+    {
+        $router
+            ->generate('transform_core_app_account')
+            ->shouldBeCalled()
+            ->willReturn('/en/account');
+
+        $this->beConstructedWith($router, true);
+
+        $event
+            ->setResponse(Argument::type('Symfony\Component\HttpFoundation\RedirectResponse'))
+            ->shouldBeCalled();
+
+        $this->onRegistrationSuccess($event);
     }
 }
