b714b07586bd6297db1687e8ce7d95151d7ee464
Eddie Jaoude <eddie@jaoudestudios.com>
2015-01-20 10:46:33 +0000

    [CSR-135] Edit Profile page with validation

--- src/TransformCore/Bundle/AppBundle/Controller/AccountController.php
+++ src/TransformCore/Bundle/AppBundle/Controller/AccountController.php
@@ -3,6 +3,9 @@
 namespace TransformCore\Bundle\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use TransformCore\Bundle\CsrFastStreamBundle\Entity\Applicant;
+use TransformCore\Bundle\CsrFastStreamBundle\Form\ProfileFormType;
 
 /**
  * Class AccountController
@@ -23,4 +26,27 @@ class AccountController extends Controller
         );
     }
 
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function profileAction(Request $request)
+    {
+        $applicant = new Applicant();
+
+        $form = $this->createForm(new ProfileFormType(), $applicant);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $applicant = $form->getData();
+
+            var_dump('valid', $applicant); exit;
+        }
+
+        return $this->render('TransformCoreAppBundle:Account:profile.html.twig',
+            array(
+                'form' => $form->createView(),
+            )
+        );
+    }
 }
