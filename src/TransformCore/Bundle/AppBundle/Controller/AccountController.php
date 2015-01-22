1607e9fdaff6364562f03fd769e60ba8cf7667e5
Dave Nash <dave.nash@teaandcode.com>
2015-01-22 12:56:52 +0000

    Added eligibility form to frontend

--- src/TransformCore/Bundle/AppBundle/Controller/AccountController.php
+++ src/TransformCore/Bundle/AppBundle/Controller/AccountController.php
@@ -3,6 +3,8 @@
 namespace TransformCore\Bundle\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use TransformCore\Bundle\CsrFastStreamBundle\Form\EligibilityFormType;
 
 /**
  * Class AccountController
@@ -23,4 +25,30 @@ class AccountController extends Controller
         );
     }
 
+    public function eligibilityAction(Request $request)
+    {
+        $form = $this->createForm(new EligibilityFormType());
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $eligibility = $form->getData();
+
+            // TODO: Save eligibility entity...
+
+            $request->getSession()
+                ->getFlashBag()
+                    ->add('success', 'Your changes were saved!');
+
+            return $this->redirect(
+                $this->generateUrl('transform_core_app_eligibility')
+            );
+        }
+
+        return $this->render(
+            'TransformCoreAppBundle:Account:eligibility.html.twig',
+            array(
+                'form' => $form->createView(),
+            )
+        );
+    }
 }
