8be9bd835f158c484a359ae2c8e23e7b3744f4a7
Jamie Burns <fantasticjamieburns@gmail.com>
2015-02-03 11:34:50 +0000

    Review controller

--- src/TransformCore/Bundle/AppBundle/Controller/AccountController.php
+++ src/TransformCore/Bundle/AppBundle/Controller/AccountController.php
@@ -4,11 +4,11 @@ namespace TransformCore\Bundle\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 
-use TransformCore\Bundle\CsrFastStreamBundle\Form\EligibilityFormType;
 use TransformCore\Bundle\CsrFastStreamBundle\Entity\Applicant;
+use TransformCore\Bundle\CsrFastStreamBundle\Form\EligibilityFormType;
 use TransformCore\Bundle\CsrFastStreamBundle\Form\ProfileFormType;
-
 
 /**
  * Class AccountController
@@ -22,9 +22,7 @@ class AccountController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('TransformCoreAppBundle:Account:index.html.twig',
-            array()
-        );
+        return $this->render('TransformCoreAppBundle:Account:index.html.twig');
     }
 
     /**
@@ -34,18 +32,19 @@ class AccountController extends Controller
      */
     public function profileAction(Request $request)
     {
-        $applicant = $this->get('transform_core_app_main.service.applicants')
-                          ->getById(
-                              $this->get('security.token_storage')
-                                   ->getToken()
-                                   ->getUser()
-                                   ->getId()
-                          );
+        if (!$this->isUserAuthenticated()) {
+            return $this->redirect(
+                $this->generateUrl('fos_user_security_login')
+            );
+        }
+
+        $applicant = $this->getApplicant();
 
         $form = $this->createForm(new ProfileFormType(), $applicant);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
+
             $applicant = $form->getData();
 
             $this->get('transform_core_app_main.service.applicants')
@@ -77,18 +76,20 @@ class AccountController extends Controller
      */
     public function eligibilityAction(Request $request)
     {
-        $applicantId = $this->get('security.token_storage')
-                                   ->getToken()
-                                   ->getUser()
-                                   ->getId();
-        
-        $eligibility = $this->get('transform_core_app_main.service.eligibility')
-                          ->getById($applicantId);
+        if (!$this->isUserAuthenticated()) {
+            return $this->redirect(
+                $this->generateUrl('fos_user_security_login')
+            );
+        }
+
+        $eligibility = $this->getEligibility();
 
         $form = $this->createForm(new EligibilityFormType(), $eligibility);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
+
+            $applicantId = $this->getApplicantId();
             $eligibility = $form->getData();
 
             $this->get('transform_core_app_main.service.eligibility')
@@ -102,7 +103,7 @@ class AccountController extends Controller
                     );
 
             return $this->redirect(
-                $this->generateUrl('transform_core_app_eligibility')
+                $this->generateUrl('transform_core_app_review')
             );
         }
 
@@ -111,5 +112,86 @@ class AccountController extends Controller
                 'form' => $form->createView(),
             )
         );
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function reviewAction()
+    {
+        if (!$this->isUserAuthenticated()) {
+            return $this->redirect(
+                $this->generateUrl('fos_user_security_login')
+            );
+        }
+
+        $validator = $this->get('validator');
+
+        $applicant = $this->getApplicant();
+        $applicantErrors = $validator->validate($applicant);
+        if (count($applicantErrors) > 0) {
+            return $this->redirect(
+                $this->generateUrl('transform_core_app_profile_edit')
+            );
+        }
+
+        $eligibility = $this->getEligibility();
+        $eligibilityErrors = $validator->validate($eligibility);
+        if (count($eligibilityErrors) > 0) {
+            return $this->redirect(
+                $this->generateUrl('transform_core_app_eligibility')
+            );
+        }
+
+        $profileForm = $this->createForm(new ProfileFormType(), $applicant);
+        $eligibilityForm = $this->createForm(new EligibilityFormType(), $eligibility);
+
+        return $this->render(
+            'TransformCoreAppBundle:Account:review.html.twig',
+            [ 'profileForm' => $profileForm->createView(),
+              'eligibilityForm' => $eligibilityForm->createView() ]
+        );
+    }
+
+    /**
+     * @return \TransformCore\Bundle\CsrFastStreamBundle\Entity\Applicant
+     */
+    private function getApplicant()
+    {
+        $applicantId = $this->getApplicantId();
+        return $this->get('transform_core_app_main.service.applicants')
+                    ->getById($applicantId);
+    }
+
+    /**
+     * @return \TransformCore\Bundle\CsrFastStreamBundle\Entity\Eligibility
+     */
+    private function getEligibility()
+    {
+        $applicantId = $this->getApplicantId();
+        return $this->get('transform_core_app_main.service.eligibility')
+                    ->getById($applicantId);
+    }
+
+    /**
+     * @return int
+     */
+    private function getApplicantId()
+    {
+        return $this->get('security.token_storage')
+                    ->getToken()
+                    ->getUser()
+                    ->getId();
+    }
+
+    /**
+     * @return bool
+     */
+    private function isUserAuthenticated()
+    {
+        $token = $this->get('security.token_storage')
+                      ->getToken();
+
+        return !($token instanceof AnonymousToken);
     }
 }
