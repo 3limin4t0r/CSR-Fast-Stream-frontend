bd0911a3d4eed2daee0c9aef4e9aab860c250108
Brett Minnie <github@bdb-studios.co.uk>
2015-02-11 14:21:43 +0000

    CSR-319 Updated controller and view

--- src/TransformCore/Bundle/AppBundle/Controller/SchemeController.php
+++ src/TransformCore/Bundle/AppBundle/Controller/SchemeController.php
@@ -4,6 +4,8 @@ namespace TransformCore\Bundle\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use TransformCore\Bundle\CsrFastStreamBundle\Entity\Scheme;
+use TransformCore\Bundle\CsrFastStreamBundle\Form\SchemeSelectionFormType;
 
 /**
  * Class SchemeController
@@ -13,9 +15,32 @@ class SchemeController extends Controller
 {
     public function selectionAction(Request $request)
     {
-       return
+        $scheme = new Scheme();
+
+        $form = $this->createForm(new SchemeSelectionFormType(), $scheme);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $program = $form->getData();
+
+            $request->getSession()
+                ->getFlashBag()
+                ->add(
+                    'success',
+                    'Your changes were saved!'
+                );
+
+            return $this->redirect(
+                $this->generateUrl('transform_core_app_account')
+            );
+        }
+
+        return
             $this->render(
-                'TransformCoreAppBundle:Scheme:selection.html.twig'
+                'TransformCoreAppBundle:Scheme:selection.html.twig',
+                array(
+                    'form' => $form->createView(),
+                )
             );
     }
 }
